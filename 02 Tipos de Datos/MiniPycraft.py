f4717f4f8c9608338fc04441261566a8983ac129
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-07-26 07:27:19 -0400

    Pycraft PJ's creados

---
+++ 02 Tipos de Datos/MiniPycraft.py
@@ -0,0 +1,29 @@
+opcion = 0
+pj = [{
+    "nombre": "Ariel, le mague",
+    "vida": 100,
+    "fuerza": 10,
+    "inteligencia": 25,
+    "defensa": 5,
+    "dado": 10,
+    "esta_vivo" : True
+},
+{
+    "nombre": "Boadicea, la guerrera",
+    "vida": 200,
+    "fuerza": 15,
+    "inteligencia": 5,
+    "defensa": 15,
+    "dado": 6,
+    "esta_vivo" : True
+},
+{
+    "nombre": "Robin, el arquero",
+    "vida": 150,
+    "fuerza": 15,
+    "inteligencia": 15,
+    "defensa": 10,
+    "dado": 8,
+    "esta_vivo" : True
+}]
+
