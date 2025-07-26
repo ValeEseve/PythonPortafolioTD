82f2efeed73b297b9ce1419667a16ca2700c3d2f
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-07-26 07:44:35 -0400

    PNJ's agregados

--- 02 Tipos de Datos/MiniPycraft.py
+++ 02 Tipos de Datos/MiniPycraft.py
@@ -1,5 +1,7 @@
+import random
+
 opcion = 0
-pj = [{
+pjs = [{
     "nombre": "Ariel, le mague",
     "vida": 100,
     "fuerza": 10,
@@ -27,3 +29,73 @@ pj = [{
     "esta_vivo" : True
 }]
 
+pnj_1 = [
+    {"nombre": "Rata",
+    "vida": 20,
+    "fuerza": 5,
+    "inteligencia": 1,
+    "defensa": 1,
+    "dado": 4,
+    "esta_vivo" : True},
+    {"nombre": "Murciélago",
+    "vida": 20,
+    "fuerza": 6,
+    "inteligencia": 2,
+    "defensa": 1,
+    "dado": 4,
+    "esta_vivo" : True},
+    {"nombre": "Baba con conciencia",
+    "vida": 50,
+    "fuerza": 6,
+    "inteligencia": 1,
+    "defensa": 3,
+    "dado": 4,
+    "esta_vivo" : True},
+]
+
+pnj_2 = [
+    {"nombre": "Pinganilla",
+    "vida": 50,
+    "fuerza": 5,
+    "inteligencia": 7,
+    "defensa": 3,
+    "dado": 4,
+    "esta_vivo" : True},
+    {"nombre": "Trampero",
+    "vida": 50,
+    "fuerza": 6,
+    "inteligencia": 5,
+    "defensa": 1,
+    "dado": 4,
+    "esta_vivo" : True},
+    {"nombre": "Mago ladrón",
+    "vida": 40,
+    "fuerza": 6,
+    "inteligencia": 10,
+    "defensa": 3,
+    "dado": 6,
+    "esta_vivo" : True},
+]
+
+pnj_boss= {
+    "nombre": "Bug, la Mandamás",
+    "vida": 150,
+    "fuerza": 6,
+    "inteligencia": 10,
+    "defensa": 3,
+    "dado": 8,
+    "esta_vivo" : True
+}
+
+pj = {
+    "nombre": "",
+    "vida": 0,
+    "fuerza": 0,
+    "inteligencia": 0,
+    "defensa": 0,
+    "dado": 0,
+    "esta_vivo" : True
+}
+
+def pj_atacar(enemigo):
+    print(f"{pj["nombre"]} ataca a {enemigo["nombre"]}.")
